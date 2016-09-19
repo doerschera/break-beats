47c66d48d9ed5768ff208e8e2a47b5c40e711641
Brad Nielsen <Brad.Nielsen@hp.local>
2016-09-19 01:02:25 -0500

    var results = response.data not working; moving on with just 'response'

--- assets/javascript/soundcloud.js
+++ assets/javascript/soundcloud.js
@@ -1,20 +1,28 @@
-function searchSoundCloud(searchGere){
-    var queryURL = "http://api.soundcloud.com/tracks?genres="+searchGere+"&client_id=72006e568cc8f77ed2a5b68c4a8c1ca5";
-
-    $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-        	console.log(queryURL);	            
-        	console.log(response);
-        });
-    return false;
-}
-
 $('#searchButton').on('click', function() {
 	event.preventDefault();
-	var	searchGere = $('#searchInput').val();
-	searchSoundCloud(searchGere);
-	console.log(searchGere);
-	alert(halp);
-	return false;
-});
 
+	var	genre = $('#searchInput').val();
+	console.log("genre " +genre);
+	var queryURL = "http://api.soundcloud.com/tracks?genres="+genre+"&client_id=72006e568cc8f77ed2a5b68c4a8c1ca5";
+	console.log("queryURL " + queryURL);
+
+
+    $.ajax({
+    		url: queryURL, 
+    		method: 'GET'
+    	})
+     	.done(function(response) {
+		
+		console.log("queryURL " + queryURL);
+		console.log(response);
+
+        var results = response.data;
+        		console.log("results" +"0");
+				console.log(response[0].comment_count);
+            for (var i = 0; i < results.length; i++) {
+				console.log("results" +i);
+				console.log(results[i]);
+            }	  		
+   	});
+});
 

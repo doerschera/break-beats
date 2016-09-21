74bfb4456e5aae45ca97c1587b60c863a771634b
Alyssa Doerscher <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-09-21 14:39:15 -0500

    added search field and firebase init

---
+++ firebase.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAauOC4PC5WrE3Br4Ff1-HkA-FxBmh1QfQ",
+    authDomain: "breakbeats-4758a.firebaseapp.com",
+    databaseURL: "https://breakbeats-4758a.firebaseio.com",
+    storageBucket: "breakbeats-4758a.appspot.com",
+    messagingSenderId: "1058322946724"
+  };
+  firebase.initializeApp(config);
+
+
+})
